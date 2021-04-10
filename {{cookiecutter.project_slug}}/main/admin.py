415028198c876b5dc75807681c2790034fdcbf0c
Enrico <enricorobazzi@gmail.com>
2021-04-10 10:35:09 -0300

    Changed website name

--- {{cookiecutter.project_slug}}/main/admin.py
+++ {{cookiecutter.project_slug}}/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-admin.site.site_header = "{{cookiecutter.project_slug}} Admin"
-admin.site.title = "{{cookiecutter.project_slug}} Admin"
+admin.site.site_header = "{{cookiecutter.project_name}} Admin"
+admin.site.title = "{{cookiecutter.project_name}} Admin"
